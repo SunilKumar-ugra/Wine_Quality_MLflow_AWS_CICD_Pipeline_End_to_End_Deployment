8b64b059bd1796df3db129aba768bceff55ed52f
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-09-17 00:28:06 +0530

    setup.py created

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Wine_Quality_MLflow_AWS_CICD_Pipeline_End_to_End_Deployment"
+AUTHOR_USER_NAME = "SunilKumar-ugra"
+SRC_REPO = "wine_quality"
+AUTHOR_EMAIL = "ugargolsunilkumar@gmail.com"
+
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for wine quality app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
