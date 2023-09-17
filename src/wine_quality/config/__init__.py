85e73678f3271e56728d48ab4ab84ec20b224fbd
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-09-17 20:20:19 +0530

    configuration done

--- src/wine_quality/config/__init__.py
+++ src/wine_quality/config/__init__.py
@@ -14,7 +14,7 @@ class ConfigurationManager:
 
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
-        self.params = read_yaml(schema_filepath)
+        self.schema = read_yaml(schema_filepath)
         
         create_directories([self.config.artifacts_root])
 
