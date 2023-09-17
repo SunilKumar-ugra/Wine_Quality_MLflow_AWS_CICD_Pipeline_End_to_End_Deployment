e682e0feb04bd8bdf8931887f3e0f687b3fe6475
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-09-17 20:51:02 +0530

    data validation done

--- src/wine_quality/config/__init__.py
+++ src/wine_quality/config/__init__.py
@@ -1,7 +1,7 @@
 from wine_quality.constants import *
 from wine_quality.utils import read_yaml, create_directories
 from wine_quality.entity import (DataIngestionConfig,
-                                 
+                                 DataValidationConfig,
                                  )
 
 class ConfigurationManager:
@@ -33,3 +33,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=config.unzip_data_dir,
+            all_schema=schema
+        )
+
+        return data_validation_config
---
+++ src/wine_quality/pipeline/stage_02_data_validation.py
@@ -0,0 +1,11 @@
+from wine_quality.components.data_validation import DataValidation
+from wine_quality.config import ConfigurationManager
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
