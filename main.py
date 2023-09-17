a4866704f568098ea8b742f7e034cd7c0a183369
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-09-17 18:41:49 +0530

    data ingestion done

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from wine_quality import logger
+from wine_quality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to wine quality app")
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise 
---
+++ src/wine_quality/components/data_ingestion.py
@@ -0,0 +1,37 @@
+import os
+import urllib.request as request
+import zipfile
+from wine_quality import logger
+from wine_quality.utils import get_size
+from wine_quality.config import DataIngestionConfig
+from pathlib import Path
+
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+
+    
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                url = self.config.source_URL,
+                filename = self.config.local_data_file
+            )
+            logger.info(f"{filename} Download! With Following Info: \n{headers}")
+        else:
+            logger.info(f"File Already Exists Of Size: {get_size(Path(self.config.local_data_file))}")  
+
+        
+    
+    def extract_zip_file(self):
+        """
+        zip_file_path: str
+        Extracts the zip file into the data directory
+        Function returns None
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
